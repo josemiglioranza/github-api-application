a32db4588c241894d358fe3d2369121ebd165843
José Guilherme Santos Miglioranza <jose.miglioranza@bandtec.com.br>
2021-10-22 10:18:20 -0300

    Create - First project structure

---
+++ app/src/main/java/com/ze/githubrepository/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ze.githubrepository.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ze.githubrepository.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
